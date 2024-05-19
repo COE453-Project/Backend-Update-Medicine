0a1f18f9e2b6cb386cfc46f4469a2f45752bc83f
Mahmoud Alrimawi <129902545+M-Alrimawi@users.noreply.github.com>
2024-05-19 16:51:14 +0300

    Connected to DB service

--- server.js
+++ server.js
@@ -4,39 +4,40 @@ const cors = require('cors');
 
 const app = express();
 const port = 3000;
+const db = 'https://backend-database-olz2xjbmza-uc.a.run.app'
 
 app.use(express.json());
 app.use(cors());
 
 app.put('/:id', (req, res, next) => {
-    // Get the details of the medicine
-    const id = req.params.id;
-    const name = req.body.name;
-    const description = req.body.description;
-    const productionDate = req.body.productionDate;
-    const expiryDate = req.body.expiryDate;
-
-    // TODO save the new details to the database
-
-    // Construct the response
-    // TODO get the storedAtTimestamp from the database
-    const riyadhTime = new Date().toLocaleString('en-US', {timeZone: 'Asia/Riyadh'});
-    const storedAtTimestamp = new Date(riyadhTime).toISOString();
-    const lastUpdatedTimestamp = new Date(riyadhTime).toISOString();
-    const response = {
-        id: id,
-        name: name,
-        description: description,
-        productionDate: productionDate,
-        expiryDate: expiryDate,
-        storedAtTimestamp: storedAtTimestamp,
-        lastUpdatedTimestamp: lastUpdatedTimestamp
-    };
-
-    // Send the response
+  // Get the details of the medicine
+  const id = req.params.id
+  const medicineData = {
+    name: req.body.name,
+    description: req.body.description,
+    productionDate:  req.body.productionDate,
+    expiryDate: req.body.expiryDate
+  };
+  const url = `${db}/${id}`
+  fetch(url, {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(medicineData)
+  })
+  .then(response => {
+    console.log(data);
+    res.status(200)
     res.json(response);
-    next();
+  })
+  .catch(error => {
+    res.status(500).send('Internal server error occurred');
+    console.error('An error occurred:', error)
+  });
+  next();
 });
+
 
 app.use(logResponse);
 
