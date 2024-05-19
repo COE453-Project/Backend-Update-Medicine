5f531b4d0a96d9c5edd69bcd028f86a4e7e2f9e9
max0man0 <0naxman0@gmail.com>
2024-05-19 16:01:34 +0300

    added cors

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const logResponse = require('./log.js');
+const cors = require('cors');
 
 const app = express();
 const port = 3000;
 
 app.use(express.json());
+app.use(cors());
 
 app.put('/:id', (req, res, next) => {
     // Get the details of the medicine
